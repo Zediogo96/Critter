bc657dea7d4119d498c76b704eff91a80aa192c6
Zé Diogo <josediogo96@outlook.com>
2023-06-22 22:16:48 +0100

    google auth now starting to work

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import Login from "./auth/Login";
 
 export default async function Nav() {
 	return (
-		<nav>
+		<nav className="flex justify-between items-center py-8">
 			<Link href={"/"}>
-				<h1> Critter. </h1>
+				<h1 className="font-bold text-lg"> Critter. </h1>
 			</Link>
+
+            <ul className="flex items-center gap-6">
+                <Login />
+            </ul>
 		</nav>
 	);
 }
