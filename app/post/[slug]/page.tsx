d66ff2b8195c033c9a233b7aa27926f6cd8d031d
Zé Diogo <josediogo96@outlook.com>
2023-06-27 00:31:34 +0100

    added initial FE for new comment in Post Page

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -5,7 +5,9 @@ import Post from "@/app/components/Post";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { PostsType } from "@/app/types/Posts";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
+
+import Image from "next/image";
 
 type URL = {
 	params: {
@@ -24,7 +26,7 @@ const fetchDetails = async (slug: string) => {
 export default function PostDetails(url: URL) {
 	const { data: session, status } = useSession();
 
-	console.log(session)
+	console.log(session);
 
 	if (status === "loading") return "Loading...";
 	if (!session) {
@@ -42,14 +44,37 @@ export default function PostDetails(url: URL) {
 
 	return (
 		<div>
-			<Post
-				key={data?.id}
-				title={data?.title || ""}
-				username={data?.user.name || ""}
-				datePublished={data?.createdAt!}
-				userImg={data?.user.image || ""}
-				id={data?.id || ""}
-			/>
+			<div>
+				<Post
+					key={data?.id}
+					title={data?.title || ""}
+					username={data?.user.name || ""}
+					datePublished={data?.createdAt!}
+					userImg={data?.user.image || ""}
+					id={data?.id || ""}
+				/>
+			</div>
+
+			<div className="flex ">
+				<Image
+					className="rounded-full w-10 h-10 mt-8 ml-5"
+					src={session.user?.image || ""}
+					alt="Profile Picture"
+					width={50}
+					height={50}
+				/>
+
+				<div className="w-full flex flex-col">
+					<textarea
+						className="w-11/12 h-20 mt-5 ml-10 p-2 rounded-md border-2 border-gray-300 focus:outline-none focus:border-blue-500"
+						placeholder="Write a comment..."
+					></textarea>
+
+					<button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 mt-3 ml-auto rounded-lg mr-6 ">
+						Reply
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
