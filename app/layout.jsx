c474c54bc50506ac73ec422a0b876661d4e28d15
Zé Diogo <josediogo96@outlook.com>
2023-06-27 00:12:28 +0100

    huge fix, using authContext to be able to get userSession on Client Side Components

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { Roboto } from "@next/font/google";
 import QueryWrapper from "@/utils/QueryWrapper";
 import Nav from "./Nav";
+import { SessionProvider } from "next-auth/react";
+import AuthContext from "./auth/AuthContext";
 
 const roboto = Roboto({
 	subsets: ["latin"],
@@ -14,8 +16,10 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-zinc-100`}>
 				<QueryWrapper>
-					<Nav />
-					{children}
+					<AuthContext>
+						<Nav />
+						{children}
+					</AuthContext>
 				</QueryWrapper>
 			</body>
 		</html>
--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
 import Post from "@/app/components/Post";
-import Image from "next/image";
+
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { PostsType } from "@/app/types/Posts";
-import { motion } from "framer-motion";
+import { useSession } from "next-auth/react"
 
 type URL = {
 	params: {
@@ -17,11 +17,21 @@ type URL = {
 // Fetch Details of a single post
 const fetchDetails = async (slug: string) => {
 	const response = await axios.get(`/api/posts/getPost/${slug}`);
-	console.log(response)
+	console.log(response);
 	return response.data.post;
 };
 
 export default function PostDetails(url: URL) {
+	const { data: session, status } = useSession();
+
+	console.log(session)
+
+	if (status === "loading") return "Loading...";
+	if (!session) {
+		// Handle the case when the user is not authenticated
+		return <div>Please sign in to view this page.</div>;
+	}
+
 	const { data, error, isLoading } = useQuery<PostsType>({
 		queryFn: () => fetchDetails(url.params.slug),
 		queryKey: ["postDetails", url.params.slug],
@@ -30,18 +40,16 @@ export default function PostDetails(url: URL) {
 	if (isLoading) return "Loading...";
 	if (error) return error;
 
-	console.log(data)
-
 	return (
-		<Post
-            key={data?.id}
-            title={data?.title || ""}
-            username={data?.user.name || ""}
-
-            datePublished={data?.createdAt!}
-            userImg={data?.user.image || ""}
-            id={data?.id || ""}
-          />
-
+		<div>
+			<Post
+				key={data?.id}
+				title={data?.title || ""}
+				username={data?.user.name || ""}
+				datePublished={data?.createdAt!}
+				userImg={data?.user.image || ""}
+				id={data?.id || ""}
+			/>
+		</div>
 	);
 }
